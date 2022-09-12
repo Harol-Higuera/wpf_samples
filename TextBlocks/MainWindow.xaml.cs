47e247de467dc6e771f34bc0076577a68b10e58c
Harol Higuera <harol.higuera@monstar-lab.com>
2022-09-12 16:52:10 +0900

    Added text block sample

---
+++ TextBlocks/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextBlocks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            topTextBlock.Foreground = Brushes.Orange;
+
+
+            // Adding TextBlock Programatically
+
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = "Hello world";
+            textBlock.Inlines.Add("This one is using inlines\n");
+            textBlock.Inlines.Add(new Run("Custom formatted text inlined here.")
+            {
+                Foreground = Brushes.Orange,
+                TextDecorations = TextDecorations.Underline
+            });
+            textBlock.TextWrapping = TextWrapping.Wrap;
+            textBlock.Foreground = Brushes.BurlyWood;
+
+            this.Content = textBlock;
+        }
+
+        private void ToGoogle_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+    }
+}
