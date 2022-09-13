d7998e4f3668c376ffd676f63d2717df3ea1b9cb
Harol Higuera <harol.higuera@monstar-lab.com>
2022-09-13 18:37:09 +0900

    CheckBoxes

---
+++ CheckBoxes/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckBoxes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void onObamaChecked(object sender, RoutedEventArgs e)
+        {
+            obamaLabel.Background = Brushes.Khaki;
+        }
+
+        private void onObamaUnchecked(object sender, RoutedEventArgs e)
+        {
+            obamaLabel.Background = Brushes.White;
+
+        }
+
+        private void cbParentChecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
