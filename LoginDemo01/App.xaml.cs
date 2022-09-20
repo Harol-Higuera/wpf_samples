165027d6dd906e294044104d064331f69660b663
Harol Higuera <harol.higuera@monstar-lab.com>
2022-09-20 22:43:29 +0900

    LOgin Demo 01

---
+++ LoginDemo01/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using LoginDemo01.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace LoginDemo01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void OnInitialized()
+        {
+            var login = Container.Resolve<Login>();
+            var result = login.ShowDialog();
+
+            // result.Value becuause result is OPTIONAL
+            //
+            if (result.Value == true)
+            {
+                // Initialize this application if the User Logs in
+                // 
+                base.OnInitialized();
+            }
+            else { 
+                // Kill this application
+                //
+                Application.Current.Shutdown();
+            }
+            
+        }
+    }
+}
