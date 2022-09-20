1f3f4ea46530b37bac07d09ea8cdbacab2635c2b
Harol Higuera <harol.higuera@monstar-lab.com>
2022-09-20 23:39:03 +0900

    Close window from a View Model

---
+++ CloseWindowFromViewModel/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+
+namespace CloseWindowFromViewModel.ViewModels
+{
+    public class MainWindowViewModel : BindableBase, ICloseWindow
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+        /// <summary>
+        ///  Declaration of the Command invoked by the Close Window Button
+        /// </summary>
+        private DelegateCommand _closeCommand;
+
+        public DelegateCommand CloseCommand => 
+            _closeCommand ?? (_closeCommand = new DelegateCommand(CloseWindow));
+
+        public Action Close { get; set; }
+
+
+
+        void CloseWindow()
+        {
+            Close?.Invoke();
+        }
+
+        public bool canClose()
+        {
+            // This is just an example, but this flag can be the result of any]
+            // logic in this View Model
+            //
+            return true;
+        }
+    }
+
+    interface ICloseWindow
+    { 
+        Action Close { get; set; }
+
+        bool canClose();
+    }
+}
---
+++ CloseWindowFromViewModel/Views/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using CloseWindowFromViewModel.ViewModels;
+using System;
+using System.Windows;
+
+namespace CloseWindowFromViewModel.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Check if the DataContext implements ICloseWindow 
+            // 
+
+            if (DataContext is ICloseWindow vm) {
+                
+                // And when the Close Action is invoked.. WE DO SOMETHING HERE
+                //
+                vm.Close += () =>
+                {
+                    this.Close();
+                };
+
+                // Listen to the Closing event of this Vindow
+                // Then, cancel the closing if the canClose is false in the View Model
+                // 
+                Closing += (s, e) =>
+                {
+                    e.Cancel = !vm.canClose();
+                };
+            }
+        }
+    }
+}
