2469615e1d21c12ac9b838ec8842aba7159289b7
Harol Higuera <harol.higuera@monstar-lab.com>
2022-09-21 09:56:07 +0900

    MVVM Dialog Demo

---
+++ MVVMDialogs/DialogService.cs
@@ -0,0 +1,24 @@
+﻿using MVVMDialogs.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDialogs
+{
+
+    public interface IDialogService {
+        void ShowDialog();
+    }
+
+    class DialogService : IDialogService
+    {
+        public void ShowDialog()
+        {
+            var dialog = new DialogWindow();
+            // Not to be confused. The following ShowDialog is from the Window component.
+            dialog.ShowDialog();
+        }
+    }
+}
---
+++ MVVMDialogs/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace MVVMDialogs.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        // In a production application, this service is injected into the constructor
+        //
+        IDialogService _dialogService = new DialogService();
+
+        private DelegateCommand _showDialog;
+
+        public DelegateCommand ShowDialog => 
+            _showDialog ?? (_showDialog = new DelegateCommand(ExecuteShowDialog));
+
+        void ExecuteShowDialog()
+        {
+            _dialogService.ShowDialog();  
+        }
+    }
+}
