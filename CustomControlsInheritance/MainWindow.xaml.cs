1db49979a7fada1c287b05b3dcc5f51aff483461
Harol Higuera <harol.higuera@monstar-lab.com>
2023-03-20 21:27:40 +0900

    Edited git ignore file

---
+++ CustomControlsInheritance/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace CustomControlsInheritance
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AnalogClock_OnTimeChanged(object sender, RoutedPropertyChangedEventArgs<DateTime> e)
+        {
+            TbTime.Text = e.NewValue.ToString("hh:mm:ss");
+        }
+    }
+}
