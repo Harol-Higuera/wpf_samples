c0ec5de2a3c3e03097c6bebfb77361078d60e357
Harol Higuera <harol.higuera@monstar-lab.com>
2023-03-20 21:46:34 +0900

    Set up project CustomControlInheritance

--- CustomControlsInheritance/Views/AnalogClock.cs
+++ CustomControlsInheritance/Views/AnalogClock.cs
@@ -1,47 +1,15 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace CustomControlsInheritance.Views
 {
-    public class AnalogClock: Control
+    public class AnalogClock: Clock
     {
         private Line? _hourHand;
         private Line? _minuteHand;
         private Line? _secondHand;
-
-        /// <summary>
-        /// Property to alter the UI from the cs View Class
-        /// Concepts:
-        ///  - DependencyProperty: Is a property for XAML controllers. Allow us to access the xaml components for styling, validation etc..
-        /// </summary>
-        public static DependencyProperty ShowSecondsProperty = DependencyProperty.Register(nameof(ShowSeconds), typeof(bool),typeof(AnalogClock), new PropertyMetadata(true));
-        // Property in the Analog Clock that hooks up with the ShowSecondsProperty
-        public bool ShowSeconds
-        {
-            get => (bool)GetValue(ShowSecondsProperty);
-            set => SetValue(ShowSecondsProperty, value);
-        }
-
-
-
-        /// <summary>
-        /// Property to send messages to the parent View
-        /// Concepts:
-        ///  - Why to use RoutedEvent?
-        ///    - Because it can be used in all the WPF Xaml Tree.Not only the element that defines it, but all the elements in the tree
-        ///  - RouterEvent gets passed through the Delegate as parameter
-        ///  - RoutingStrategy.Direct : Only the AnalogClock can change the events
-        /// </summary>
-        public static RoutedEvent TimeChangedEvent = EventManager.RegisterRoutedEvent("TimeChanged", RoutingStrategy.Direct, typeof(RoutedPropertyChangedEventHandler<DateTime>), typeof(AnalogClock));
-        public event RoutedPropertyChangedEventHandler<DateTime> TimeChanged
-        {
-            add => AddHandler(TimeChangedEvent, value);
-            remove => RemoveHandler(TimeChangedEvent, value);
-        }
 
         static AnalogClock()
         {
@@ -69,44 +37,15 @@ namespace CustomControlsInheritance.Views
             //};
             //_secondHand?.SetBinding(VisibilityProperty, showSecondHandBinding);
 
-            UpdateHandAngles(DateTime.Now);
-
-            // Implement update the handles every single second
-            DispatcherTimer timer = new DispatcherTimer
-            {
-                Interval = new TimeSpan(0, 0, 1)
-            };
-            timer.Tick += (sender, args) => OnTimeChanged(DateTime.Now);
-            timer.Start();
-            
             base.OnApplyTemplate();
         }
 
         // virtual. Makes it overridable
-        protected virtual void OnTimeChanged(DateTime time)
+        protected override void OnTimeChanged(DateTime time)
         {
-            // 1:::: Update teh UI
+            // 1:::: Update the UI
             UpdateHandAngles(time);
-            
-            // 2:::: Update TimeState
-            UpdateTimeState(time);
-
-            // 3:::: Rise the event so the Parent View understands
-            // TODO: Handle Old and New Value in a better way 
-            RaiseEvent(new RoutedPropertyChangedEventArgs<DateTime>(DateTime.Now.AddSeconds(-1), DateTime.Now, TimeChangedEvent));
-        }
-
-        private void UpdateTimeState(DateTime time)
-        {
-            // Correct condition: if (time.Hour is > 6 and < 18)
-            if (time.Second is > 10 and < 40)
-            {
-                VisualStateManager.GoToState(this, "Day", false);
-            }
-            else
-            {
-                VisualStateManager.GoToState(this, "Night", false);
-            }
+            base.OnTimeChanged(time);
         }
 
         private void UpdateHandAngles(DateTime time)
---
+++ CustomControlsInheritance/Views/Clock.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace CustomControlsInheritance.Views
+{
+    public class Clock: Control
+    {
+        /// <summary>
+        /// Property to alter the UI from the cs View Class
+        /// Concepts:
+        ///  - DependencyProperty: Is a property for XAML controllers. Allow us to access the xaml components for styling, validation etc..
+        /// </summary>
+        public static DependencyProperty ShowSecondsProperty = DependencyProperty.Register(nameof(ShowSeconds), typeof(bool), typeof(Clock), new PropertyMetadata(true));
+        // Property in the Analog Clock that hooks up with the ShowSecondsProperty
+        public bool ShowSeconds
+        {
+            get => (bool)GetValue(ShowSecondsProperty);
+            set => SetValue(ShowSecondsProperty, value);
+        }
+
+        /// <summary>
+        /// Property to send messages to the parent View
+        /// Concepts:
+        ///  - Why to use RoutedEvent?
+        ///    - Because it can be used in all the WPF Xaml Tree.Not only the element that defines it, but all the elements in the tree
+        ///  - RouterEvent gets passed through the Delegate as parameter
+        ///  - RoutingStrategy.Direct : Only the AnalogClock can change the events
+        /// </summary>
+        public static RoutedEvent TimeChangedEvent = EventManager.RegisterRoutedEvent("TimeChanged", RoutingStrategy.Direct, typeof(RoutedPropertyChangedEventHandler<DateTime>), typeof(Clock));
+        public event RoutedPropertyChangedEventHandler<DateTime> TimeChanged
+        {
+            add => AddHandler(TimeChangedEvent, value);
+            remove => RemoveHandler(TimeChangedEvent, value);
+        }
+
+        public override void OnApplyTemplate()
+        {
+
+            OnTimeChanged(DateTime.Now);
+
+            // Implement update the handles every single second
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 1)
+            };
+            timer.Tick += (sender, args) => OnTimeChanged(DateTime.Now);
+            timer.Start();
+
+            base.OnApplyTemplate();
+        }
+
+        // virtual. Makes it overridable
+        protected virtual void OnTimeChanged(DateTime time)
+        {
+            // 2:::: Update TimeState
+            UpdateTimeState(time);
+
+            // 3:::: Rise the event so the Parent View understands
+            // TODO: Handle Old and New Value in a better way 
+            RaiseEvent(new RoutedPropertyChangedEventArgs<DateTime>(DateTime.Now.AddSeconds(-1), DateTime.Now, TimeChangedEvent));
+        }
+
+        private void UpdateTimeState(DateTime time)
+        {
+            // Correct condition: if (time.Hour is > 6 and < 18)
+            if (time.Second is > 10 and < 40)
+            {
+                VisualStateManager.GoToState(this, "Day", false);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "Night", false);
+            }
+        }
+    }
+}
