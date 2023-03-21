2730c51bd7232715a1672d539a3661efb3600039
Harol Higuera <harol.higuera@monstar-lab.com>
2023-03-21 14:56:31 +0900

    Implements Composition Over Inheritance

---
+++ CustomControlsInheritance/Views/ControllableClock.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomControlsInheritance.Views
+{
+    /// <summary>
+    /// Interaction logic for ControllableClock.xaml
+    /// </summary>
+    public partial class ControllableClock : UserControl
+    {
+        public ControllableClock()
+        {
+            InitializeComponent();
+        }
+    }
+}
